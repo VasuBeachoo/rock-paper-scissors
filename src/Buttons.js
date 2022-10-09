516a35003d10737d1be467e3cc64ef388e75c8b6
VasuBeachoo <vasubeachoo@gmail.com>
2022-10-09 17:48:03 -0400

    Style buttons

--- src/Buttons.js
+++ src/Buttons.js
@@ -1,3 +1,23 @@
 import styled from "styled-components";
 
-export const WhiteOutlineBtn = styled.button``;
+export const WhiteOutlineBtn = styled.button`
+  all: unset;
+  text-align: center;
+  user-select: none;
+  background: transparent;
+  color: var(--White);
+  font-size: 1.1rem;
+  font-weight: 600;
+  letter-spacing: 0.5ch;
+  line-height: 2.75ch;
+  border: 0.1rem solid var(--White);
+  border-radius: 0.5rem;
+  padding: 1rem 1.75rem;
+  transition: 0.25s;
+
+  &:hover {
+    cursor: pointer;
+    background-color: var(--White);
+    color: var(--Score-text);
+  }
+`;
